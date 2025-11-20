4039edf73679815b249f39afe566f2bd3cf2a331
zakia <zoyabibi536@gmail.com>
2025-11-20 09:27:30 +0500

    Updated something

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
     <h1>Hello World from React!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsum ipsa, eos ad, placeat incidunt veniam officia, aspernatur quisquam atque commodi maiores?</p>
   );
 }
 
