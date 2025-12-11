4b8bb26efc29a24e63b0c9a491d40a68967bf7cd
zakia <zoyabibi536@gmail.com>
2025-12-11 22:10:46 +0500

    Update Register.jsx with loading and error handling

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,13 +3,33 @@ import { useEffect, useState } from "react";
 function Register() {
 
  const [students,setStudents]=useState([])
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); 
+
 
  useEffect(()=>{
 fetch("https://jsonplaceholder.typicode.com/todos")
-.then(res=>res.json())
-.then(info=>setStudents(info))
-
+.then(res=>{
+  if(!res.ok) throw new Error("something Wrong")
+    return res.json()
+})
+.then(info=>{setStudents(info);
+setLoading(false);
+ })
+.catch(error=>{
+  setError(error.message);
+   setLoading(false);
+ 
+})
  },[]);
+
+ if(loading){
+  return <h2>Loading....</h2>
+ }
+
+ if(error){
+  return <h2> Error:{error} </h2>
+ }
 
 
  return(
